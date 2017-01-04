ba6970df2db63a695409a34c1617b1b1f20b93e0
Theodor Diaconu <diaconu.theodor@gmail.com>
2017-01-04 11:29:14 +0200

    bump to 1.2.6

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'cultofcoders:grapher',
-    version: '1.2.5_2',
+    version: '1.2.6',
     // Brief, one-line summary of the package.
     summary: 'Grapher makes linking collections easily. And fetching data as a graph.',
     // URL to the Git repository containing the source code for this package.
