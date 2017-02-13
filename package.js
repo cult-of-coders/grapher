687fa2fbd766a1d9504fca069096184fc71ea759
jens <jens.verbeken@redandivory.com>
2017-02-13 15:20:30 +0100

    version bump because meteor 1.4.3 named classes

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'cultofcoders:grapher',
-    version: '1.2.7',
+    version: '1.2.8',
     // Brief, one-line summary of the package.
     summary: 'Grapher makes linking collections easily. And fetching data as a graph.',
     // URL to the Git repository containing the source code for this package.
