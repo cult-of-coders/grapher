45a2080ff484eb2c87d704754b54ffa11603e925
Aaron <aaronlo207@gmail.com>
2018-09-21 19:31:36 -0400

    TS types for grapher

--- node_modules/@types/meteor/service-configuration.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "meteor/service-configuration" {
-    interface Configuration {
-        appId: string;
-        secret: string;
-    }
-    var ServiceConfiguration: {
-        configurations: Mongo.Collection<Configuration>;
-    }
-}
--- node_modules/@types/meteor/templating.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "meteor/templating" {
-    var Template: TemplateStatic;
-    interface TemplateStatic extends Blaze.TemplateStatic {
-        new (viewName?: string, renderFunction?: Function): Blaze.Template;
-        body: Blaze.Template;
-        [index: string]: any | Blaze.Template;
-    }
-}
