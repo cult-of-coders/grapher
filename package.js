ee80f4e57e6d545357e3cd1f88db2b3eb5cf6f40
Don Stephan <donald.m.stephan@gmail.com>
2021-10-29 15:51:06 -0400

    updated package.js, updated npm packages, fixed broken tests
    - note that sift was quite a few versions back and includes breaking changes, should this be version bumped because of this?

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "cultofcoders:grapher",
-  version: "1.3.19",
+  version: "1.3.20",
   // Brief, one-line summary of the package.
   summary: "Grapher is a data fetching layer on top of Meteor",
   // URL to the Git repository containing the source code for this package.
@@ -10,14 +10,16 @@ Package.describe({
   documentation: "README.md"
 });
 
-Npm.depends({
-  sift: "3.2.6",
-  "dot-object": "1.5.4",
+const npmPackages = {
+  "sift": "15.0.0",
+  "dot-object": "1.9.0",
   "lodash.clonedeep": "4.5.0",
-  "deep-extend": "0.5.0"
-});
+  "deep-extend": "0.6.0",
+}
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
+  Npm.depends(npmPackages);
+
   api.versionsFrom(["1.3", "2.3"]);
 
   var packages = [
@@ -31,7 +33,7 @@ Package.onUse(function(api) {
     "reywood:publish-composite@1.7.3",
     "dburles:mongo-collection-instances@0.3.5",
     "peerlibrary:subscription-scope@0.5.0",
-    "herteby:denormalize@0.6.5"
+    "herteby:denormalize@0.6.6"
   ];
 
   api.use(packages);
@@ -40,8 +42,13 @@ Package.onUse(function(api) {
   api.mainModule("main.server.js", "server");
 });
 
-Package.onTest(function(api) {
+Package.onTest(function (api) {
   api.use("cultofcoders:grapher");
+  
+  Npm.depends({
+    ...npmPackages,
+    chai: "4.3.4"
+  });
 
   var packages = [
     "random",
@@ -50,7 +57,7 @@ Package.onTest(function(api) {
     "matb33:collection-hooks@1.1.0",
     "reywood:publish-composite@1.7.3",
     "dburles:mongo-collection-instances@0.3.5",
-    "herteby:denormalize@0.6.5",
+    "herteby:denormalize@0.6.6",
     "mongo"
   ];
 
