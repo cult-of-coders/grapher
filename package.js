b4b71173dc0e6f684f19fd18fef61f5bc2b53d7e
Jan Dvorak <storytellercz@proton.me>
2023-05-07 11:27:48 +0200

    Meteor 2.3.1

--- package.js
+++ package.js
@@ -20,7 +20,7 @@ const npmPackages = {
 Package.onUse(function (api) {
   Npm.depends(npmPackages);
 
-  api.versionsFrom(["2.3.6", "2.6.1", "2.7.3", "2.8.1", "2.9.1"]);
+  api.versionsFrom(["2.3.1", "2.6.1", "2.7.3", "2.8.1", "2.9.1"]);
 
   var packages = [
     "ecmascript",
