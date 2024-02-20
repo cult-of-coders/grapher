eb9b062f409cb5b8dae65e11430b94258ccab19c
Bero <bero@stillwater.dev>
2024-02-20 08:45:55 +0100

    Eslint setup with TS type checking

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    // 'plugin:@typescript-eslint/recommended',
+    // Going with type checked because of https://typescript-eslint.io/rules/no-floating-promises
+    'plugin:@typescript-eslint/recommended-type-checked',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  globals: {
+    _: true, // TODO: replace with explicit imports
+    Meteor: true,
+    Npm: true,
+    Package: true,
+    Mongo: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: true,
+  },
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['test*'],
+  rules: {
+    // For now
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
+  },
+};
